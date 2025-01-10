4f41c93a5763712625294bd3656e72f975e74c41
jitender1322 <jitendersarswat6@gmail.com>
2025-01-10 13:49:18 +0530

    practice

--- Project-4/pr.py
+++ Project-4/pr.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def summary(data):
     print("Data Summary")
     print(f"Total elements : {len(data)}")
@@ -36,6 +38,33 @@ while True:
         for i in range(1,number+1) :
             a *= i
         print(a)
+    elif choice == 4:
+        number= int(input("Enter a number to filter out data below this value : "))
+        filtered = []
+        for i in range(len(numList)):
+            if numList[i]>=number:
+                filtered.append(numList[i])
+            else:
+                pass
+        print(filtered)
+
+    elif choice == 5:
+        print("Choose sorting method :")
+        print("1. Ascending")
+        print("2. Descending")
+        number= int(input("Enter your choice : "))
+        if number == 1 :
+            numList.sort()
+        elif number == 2 :
+            numList.sort(reverse=True)
+        print(numList)
+
+    elif choice == 6:
+        print("Dataset Statistics :")
+        print(f"Minimum Value : {min(numList)}")
+        print(f"Maximum Value : {max(numList)}")
+        print(f"Sum of all Values : {sum(numList)}")
+        print(f"Average Values : {np.mean(numList)}")
 
     elif choice == 7:
         print("Thank you !")
